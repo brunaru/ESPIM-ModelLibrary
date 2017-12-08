c95c5004644216651d5395e3973bf45ccb80a822
Brunaru <brunaru7@gmail.com>
2017-12-08 00:46:09 -0200

    Nome do parâmetro de resposta

--- src/main/java/br/usp/icmc/intermidia/esm/rest/api/client/model/intervention/TaskIntervention.java
+++ src/main/java/br/usp/icmc/intermidia/esm/rest/api/client/model/intervention/TaskIntervention.java
@@ -4,6 +4,8 @@ public class TaskIntervention extends Intervention {
 	
 	private static final long serialVersionUID = -5178905488082755947L;
 	
+	public static final String URL_FOR_DATA_FILE = "urlForDataFile";
+	
 	private String appPackage;
 	
 	public TaskIntervention() {
