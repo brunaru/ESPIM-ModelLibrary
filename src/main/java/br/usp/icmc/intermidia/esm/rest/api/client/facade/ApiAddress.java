00f02c544a853cad22c947d1af206e230bbd6ae5
Brunaru <brunaru7@gmail.com>
2018-03-20 16:31:15 -0300

    api address

--- src/main/java/br/usp/icmc/intermidia/esm/rest/api/client/facade/ApiAddress.java
+++ src/main/java/br/usp/icmc/intermidia/esm/rest/api/client/facade/ApiAddress.java
@@ -3,8 +3,7 @@ package br.usp.icmc.intermidia.esm.rest.api.client.facade;
 public class ApiAddress {
 	
 	/** Default value. */
-	//private static final String REST_API_ADDRESS = "http://www.espim.com.br/ws-0.1/";
-	private static final String REST_API_ADDRESS = "http://192.168.0.111:8000/";
+	private static final String REST_API_ADDRESS = "http://www.espim.com.br/ws-0.1/";
 	
 	private static String restApiAddress = REST_API_ADDRESS;
 
